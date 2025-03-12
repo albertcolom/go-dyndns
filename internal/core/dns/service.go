161a263bc8364935d087c68990941b6f770c3d0d
Albert Colom <skolom@gmail.com>
2025-03-12 23:32:18 +0100

    Mockgen as a tool

--- internal/core/dns/service.go
+++ internal/core/dns/service.go
@@ -1,4 +1,4 @@
-//go:generate mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
+//go:generate go run go.uber.org/mock/mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
 
 package dns
 
